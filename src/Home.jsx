dac98cf6113b849d1ac17d51c164298babf65348
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-01-20 21:50:22 +0530

    video

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { NavLink } from "react-router-dom";
 
-const Home =() => {
+
+const Home = () => {
   return (
     <>
-      <video src={process.env.PUBLIC_URL + './Earth.mp4'} autoPlay muted loop className="video"></video>
-      <section className="container-fluid">
+      <div className="txt">
+        <video src={process.env.PUBLIC_URL + './Earth.mp4'} autoPlay muted loop className="video"></video>
+          <NavLink to="/" className="navbar-brand nav-link logo">
+          <img src={process.env.PUBLIC_URL + './logo192.png'} alt="logo" />
+        </NavLink>
+        <div className="label" style={{textTransform: 'uppercase'}}>
+          <h1>Aerometic</h1>
+          <h1>Care</h1>
+        </div>
+        <NavLink to="/" className="scroll">Know More</NavLink>
+      </div>
+      <section className="container-fluid home">
         <Navbar />
         <h1>Welcome to Homepage</h1>
         <h1>Welcome to Homepage</h1>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,27 +5,31 @@ import "./scroll"
 const Navbar = () => {
     return (
         <>
-            <div id="myTopnav">
-                <nav className="navbar navbar-expand-lg navbar-light mx-auto">
-                    <NavLink to="/" className="navbar-brand nav-link">
-                        <img src={process.env.PUBLIC_URL + './favicon.ico'} alt="logo" />
-                    </NavLink>
+            <div id="myTopnav" className="topnav">
+                <nav className="navbar navbar-expand-lg navbar-light">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse categories" id="navbarNav">
+                        <ul className="navbar-nav menu">
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to="/products" className="nav-link">Products</NavLink>
                             </li>
+                        </ul>
+                    </div>
+                    <NavLink to="/" className="navbar-brand nav-link logo">
+                        <img src={process.env.PUBLIC_URL + './logo_1.png'} alt="logo" />
+                    </NavLink>
+                    <div className="collapse navbar-collapse categories" id="navbarNav">
+                        <ul className="navbar-nav menu1">
                             <li className="nav-item">
-                                <NavLink to="/about" className="nav-link">About US</NavLink>
+                                <NavLink to="/about" className="nav-link">About Us</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/contact" className="nav-link">Contact US</NavLink>
+                                <NavLink to="/contact" className="nav-link">Contact Us</NavLink>
                             </li>
                         </ul>
                     </div>
