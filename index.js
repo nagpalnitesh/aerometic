30cf10860829a43dc7009a176e880410cc08be7c
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-01-22 22:20:03 +0530

    server

---
+++ index.js
@@ -0,0 +1,64 @@
+/* Imports */
+const express=require('express');
+const bodyparser= require('body-parser');
+const cors = require('cors');
+const nodemailer=require('nodemailer');
+
+/* Mailer settings */
+async function xyz(){
+  
+return transporter;
+}
+
+/* Server initialize */
+const app=express();
+
+/* Sets and uses */
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cors());
+
+/* Routes */
+app.get('/',(req,res)=>{
+    res.end('Server up and running');
+})
+app.post('/sendmail',async (req,res)=>{
+    console.log('requested');
+    var name = req.body.name;
+    var email = req.body.email;
+    var message = req.body.message;
+    var phone = req.body.phone;
+    var transporter = nodemailer.createTransport({
+  host: "smtpout.secureserver.net",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: 'contact@pixxmo.com', // generated ethereal user
+    pass: 'Lamborghini@1115218115', // generated ethereal password
+  },
+});
+
+    var mailOptions = {
+        from: 'contact@pixxmo.com',
+        to: 'pixxmoworld@gmail.com',
+        subject: 'Query form filled',
+        html: `<b>From</b>:${name} (${email})<br><b>Message</b>: ${message}<br><b>Contact</b>: ${phone}`
+    };
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if (error) {
+          console.log(error);
+          res.send(JSON.stringify({info:error,type:'error'}));
+          res.end();
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send(JSON.stringify({info:info,type:'info'}));
+          res.end();
+        }
+    });
+})
+
+/* Ports and listens */
+var port = process.env.PORT
+app.listen(port,'0.0.0.0',()=>{
+    console.log(`port : ${port}`)
+});
